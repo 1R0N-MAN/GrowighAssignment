b0ffe7760325f72ef8d867041485a12ed0e20e7f
1R0N-MAN <praiseoladejo2003@gmail.com>
2023-07-22 13:45:38 +0100

    Added Home Page

--- app/src/main/java/com/example/growighassignment/HomeActivity.kt
+++ app/src/main/java/com/example/growighassignment/HomeActivity.kt
@@ -1,11 +1,27 @@
 package com.example.growighassignment
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val btnFeeds = findViewById<Button>(R.id.btnFeeds)
+        btnFeeds.setOnClickListener { openFeedsPage() }
+
+        val btnUploadImage = findViewById<Button>(R.id.btnUploadImage)
+        btnUploadImage.setOnClickListener { openUploadImagePage() }
+    }
+
+    private fun openFeedsPage() {
+        startActivity(Intent(this, FeedsActivity::class.java))
+    }
+
+    private fun openUploadImagePage() {
+        startActivity(Intent(this, UploadImageActivity::class.java))
     }
 }
